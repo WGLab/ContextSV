af1e35db896b006280250d394604413f10782971
perdomoj <jonperdomodb@gmail.com>
2022-09-09 15:12:43 -0400

    STYLE: Add help text

--- src/gensv.cpp
+++ src/gensv.cpp
@@ -3,8 +3,23 @@
 //
 
 #include <iostream>
+#include <cstring>
+
 
 int main(int argc, char *argv[]){
-    std::cout << "Hello World!" << std::endl;
+    // Print help text
+    if(argc == 2 && (strcmp(argv[1], "-h")==0 || strcmp(argv[1], "--help")==0))
+    {
+        std::string help_text(
+                "\n ===== GenSV =====\n"
+                "\nPositional arguments:\n"
+                "-i      BAM file input"
+                "-o      Output directory\n"
+                "\nOptional arguments:\n"
+                "-h, --help     Show this help message and exit\n"
+                "\nExample:\n"
+                "./gensv -i path/to/input.bam -o /output_directory/");
+        std::cout << help_text << std::endl;
+    }
     return 0;
 }
