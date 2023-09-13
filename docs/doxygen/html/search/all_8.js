070f47e0d583f808608c1ce64b286071e70a59d3
jonperdomo <jonperdomodb@gmail.com>
2023-09-13 13:01:49 -0400

    Update docs

--- docs/doxygen/html/search/all_8.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['handlers_0',['handlers',['../namespace____main____.html#a092782ad861e46175be9d2f0dd0d8f85',1,'__main__.handlers'],['../namespacecnv__plots.html#ac51a34af541af387b38b86f1cdbd5c26',1,'cnv_plots.handlers']]],
-  ['hg002_20data_3a_1',['Use minimap2 to self-align the hg002 data:',['../md_doc_2genome-assembly-guide.html#autotoc_md4',1,'']]],
-  ['hybrid_20optical_20map_20guided_20assembly_2',['Hybrid Optical Map-Guided Assembly',['../md_doc_2genome-assembly-guide.html',1,'']]]
-];
